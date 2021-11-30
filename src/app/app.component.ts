74234d128a856e376763d19ab0b77353a36e9ca4
Bibi <bibi.aysha1@t-mobile.com>
2021-11-30 16:39:29 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pa-starter-angular';
+  nameModel: string = "";
+  instructions: string = "";
+
+  listOfNames = Array();
+  display = true;
+  variable = true;
+  showContent() {
+    this.variable = false;
+  }
+
+  onSubmit() {
+    this.listOfNames.push(this.nameModel)
+    console.log(this.listOfNames);
+    this.nameModel = '';
+    this.instructions = '';
+
+  }
+
+
 }
